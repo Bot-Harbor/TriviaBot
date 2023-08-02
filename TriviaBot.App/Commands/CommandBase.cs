0d8bca10627566f802645e76dcc0ee43d1a85d3e
Kaleb Garrett <kalebagarrett@gmail.com>
2023-08-02 19:28:16 -0400

    Added Ping Command

---
+++ TriviaBot.App/Commands/CommandBase.cs
@@ -0,0 +1,8 @@
+﻿using Discord.Commands;
+
+namespace TriviaBot.App.Commands;
+
+public class CommandBase : ModuleBase<SocketCommandContext>
+{ 
+    protected string Mention => Context.Message.Author.Mention;
+}
---
+++ TriviaBot.App/Commands/PingCommand.cs
@@ -0,0 +1,12 @@
+﻿using Discord.Commands;
+
+namespace TriviaBot.App.Commands;
+
+public class PingCommand : CommandBase
+{
+    [Command("ping")]
+    public async Task Ping()
+    {
+        await ReplyAsync("Pinging to server...");
+    }
+}
