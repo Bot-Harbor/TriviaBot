83487939014045bfbc2a9f8103379c722ca5abff
Kaleb Garrett <kalebagarrett@gmail.com>
2023-08-07 20:14:44 -0400

    Updated timer layout

--- TriviaBot.App/Commands/TriviaTemplate.cs
+++ TriviaBot.App/Commands/TriviaTemplate.cs
@@ -1,14 +1,13 @@
 ﻿using Discord;
 using Discord.Commands;
 using RestSharp;
-
 using TriviaBot.App.Models;
 
 namespace TriviaBot.App.Commands
 {
     public class TriviaTemplate : CommandBase
     {
-        private static readonly Random random = new Random();
+        private static readonly Random Random = new Random();
 
         [Command("video games")]
         public async Task TriviaCommandAsync()
@@ -26,7 +25,7 @@ namespace TriviaBot.App.Commands
                     var decodedCorrectAnswer = DecodeHtml(triviaQuestion.Correct_Answer);
                     var decodedAllAnswers = triviaQuestion.AllAnswers.Select(DecodeHtml).ToList();
 
-                    var answerChoices = new List<string> { "A", "B", "C", "D" };
+                    var answerChoices = new List<string> {"A", "B", "C", "D"};
                     var shuffledChoices = ShuffleAnswers(decodedAllAnswers);
                     var buttons = new ComponentBuilder();
 
@@ -43,29 +42,40 @@ namespace TriviaBot.App.Commands
                         .WithTitle($"Category: {triviaQuestion.Category}")
                         .WithDescription($"**Difficulty: **{triviaQuestion.Difficulty.ToUpper()}")
                         .AddField("Question:", decodedQuestion, inline: false)
+                        .WithFooter("You have 15 seconds to answer.")
                         .WithImageUrl(
                             "https://images.squarespace-cdn.com/content/v1/5a328d66a8b2b051a8d2f017/1567530555218-OF3Y7UYVG767NHSMP46D/trivianight.gif")
                         .Build();
 
                     await ReplyAsync(embed: questionEmbed, components: components);
-                    
+
+                    await Task.Delay(TimeSpan.FromSeconds(3));
+
                     var fiveSecCountEmbed = new EmbedBuilder()
-                        .WithColor(Color.DarkRed)
-                        .WithImageUrl("https://i.gifer.com/origin/07/07dbafd836794dc3e165ef798aae5b59_w200.gif")
+                        .WithTitle("Time Remaining: ")
+                        .WithColor(Color.Gold)
                         .Build();
 
-                    await Task.Delay(TimeSpan.FromSeconds(10));
-                    await ReplyAsync(embed: fiveSecCountEmbed);
-                    
+                    var message = await ReplyAsync(embed: fiveSecCountEmbed);
+
+                    for (var i = 5; i >= 0; i--)
+                    {
+                        fiveSecCountEmbed = new EmbedBuilder()
+                            .WithTitle($"Time Remaining: {i}")
+                            .WithColor(Color.Gold)
+                            .Build();
+
+                        await message.ModifyAsync(properties => properties.Embed = fiveSecCountEmbed);
+                        await Task.Delay(TimeSpan.FromSeconds(1));
+                    }
+
                     var timeUpEmbed = new EmbedBuilder()
                         .WithColor(Color.Red)
                         .WithTitle("⌛ Time's Up!")
-                        .WithDescription($"The correct answer is: **{decodedCorrectAnswer}**")
+                        .WithDescription($"The correct answer is: **{Environment.NewLine}{decodedCorrectAnswer}**")
                         .Build();
-                    
-                    await Task.Delay(TimeSpan.FromSeconds(5));
-                    await ReplyAsync(embed: timeUpEmbed);
 
+                    await ReplyAsync(embed: timeUpEmbed);
                 }
                 else
                 {
@@ -76,19 +86,19 @@ namespace TriviaBot.App.Commands
 
         private string DecodeHtml(string rawHtml)
         {
-            string decodedQuestion = System.Net.WebUtility.HtmlDecode(rawHtml);
-            string cleanedQuestion = decodedQuestion.Replace("\"", "");
+            var decodedQuestion = System.Net.WebUtility.HtmlDecode(rawHtml);
+            var cleanedQuestion = decodedQuestion.Replace("\"", "");
 
             return cleanedQuestion;
         }
 
         private List<T> ShuffleAnswers<T>(List<T> list)
         {
-            int n = list.Count;
+            var n = list.Count;
             while (n > 1)
             {
                 n--;
-                int k = random.Next(n + 1);
+                var k = Random.Next(n + 1);
                 (list[k], list[n]) = (list[n], list[k]);
             }
 
