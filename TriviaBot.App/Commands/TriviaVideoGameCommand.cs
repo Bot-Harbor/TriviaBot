e7adc541ea73a074096c96aac60827d6d2bb6dce
Kaleb Garrett <kalebagarrett@gmail.com>
2023-08-10 16:32:24 -0400

    Added video game command

---
+++ TriviaBot.App/Commands/TriviaVideoGameCommand.cs
@@ -0,0 +1,14 @@
+﻿using Discord;
+using Discord.Commands;
+
+namespace TriviaBot.App.Commands;
+
+public class TriviaVideoGameCommand : TriviaCommandHandler
+{
+    [Command("video games")]
+    public async Task SportsCommandAsync()
+    {
+        await TriviaCommandAsync("https://opentdb.com/api.php?amount=1&category=15&type=multiple", "Entertainment: Video Games",
+            Color.Purple);
+    }
+}
