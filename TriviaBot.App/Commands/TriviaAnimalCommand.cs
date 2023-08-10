072f62fc0c352f82945adc8e64531966048f886f
Kaleb Garrett <kalebagarrett@gmail.com>
2023-08-10 16:41:21 -0400

    Added animal command

---
+++ TriviaBot.App/Commands/TriviaAnimalCommand.cs
@@ -0,0 +1,14 @@
+﻿using Discord;
+using Discord.Commands;
+
+namespace TriviaBot.App.Commands;
+
+public class TriviaAnimalCommand : TriviaCommandHandler
+{
+    [Command("animal")]
+    public async Task SportsCommandAsync()
+    {
+        await TriviaCommandAsync("https://opentdb.com/api.php?amount=1&category=27&type=multiple", "Animals",
+            Color.Gold);
+    }
+}
