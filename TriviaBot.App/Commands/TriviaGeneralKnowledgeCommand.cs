566addf77e970a339fe05eca978e6a4230bea7cc
Kaleb Garrett <kalebagarrett@gmail.com>
2023-08-10 16:36:48 -0400

    Added general knowledge command

---
+++ TriviaBot.App/Commands/TriviaGeneralKnowledgeCommand.cs
@@ -0,0 +1,14 @@
+﻿using Discord;
+using Discord.Commands;
+
+namespace TriviaBot.App.Commands;
+
+public class TriviaGeneralKnowledgeCommand : TriviaCommandHandler
+{
+    [Command("general knowledge")]
+    public async Task SportsCommandAsync()
+    {
+        await TriviaCommandAsync("https://opentdb.com/api.php?amount=1&category=9&type=multiple", "General Knowledge",
+            Color.LightGrey);
+    }
+}
