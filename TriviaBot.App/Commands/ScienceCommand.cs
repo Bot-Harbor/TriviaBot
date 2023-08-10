9580b94bacade9617345e78b14c56630fcfa09c2
Kaleb Garrett <kalebagarrett@gmail.com>
2023-08-10 16:48:56 -0400

    Added science command

---
+++ TriviaBot.App/Commands/ScienceCommand.cs
@@ -0,0 +1,14 @@
+﻿using Discord;
+using Discord.Commands;
+
+namespace TriviaBot.App.Commands;
+
+public class ScienceCommand : TriviaCommandHandler
+{
+    [Command("science")]
+    public async Task SportsCommandAsync()
+    {
+        await TriviaCommandAsync("https://opentdb.com/api.php?amount=1&category=17&type=multiple", "Science & Nature",
+            Color.DarkRed);
+    }
+}
