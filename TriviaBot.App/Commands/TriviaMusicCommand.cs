925fc94752a7947b1978a444815d2ad6b35f6898
Kaleb Garrett <kalebagarrett@gmail.com>
2023-08-10 16:46:03 -0400

    Added music command

---
+++ TriviaBot.App/Commands/TriviaMusicCommand.cs
@@ -0,0 +1,14 @@
+﻿using Discord;
+using Discord.Commands;
+
+namespace TriviaBot.App.Commands;
+
+public class TriviaMusicCommand : TriviaCommandHandler
+{
+    [Command("music")]
+    public async Task SportsCommandAsync()
+    {
+        await TriviaCommandAsync("https://opentdb.com/api.php?amount=1&category=12&type=multiple", "Entertainment: Music",
+            Color.Magenta);
+    }
+}
