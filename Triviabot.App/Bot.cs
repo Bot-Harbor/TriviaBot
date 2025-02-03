4af01ed6605b373f4d0d11bf8f3484e986ee1edd
Kaleb Garrett <kalebagarrett@gmail.com>
2025-02-03 16:33:51 -0500

    Configured bot

---
+++ Triviabot.App/Bot.cs
@@ -0,0 +1,21 @@
+﻿using DSharpPlus;
+using Microsoft.Extensions.Hosting;
+
+namespace Triviabot.App;
+
+public class Bot : BackgroundService
+{
+    private readonly DiscordClient _client;
+    private readonly IServiceProvider _serviceProvider;
+    
+    public Bot(DiscordClient client, IServiceProvider serviceProvider)
+    {
+        _client = client;
+        _serviceProvider = serviceProvider;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await _client.ConnectAsync();
+    }
+}
--- Triviabot.App/Program.cs
+++ Triviabot.App/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DSharpPlus;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Triviabot.App;
+using Triviabot.App.Secrets;
 
-Console.WriteLine("Hello, World!");
+var builder = new HostApplicationBuilder();
+builder.Services.AddHostedService<Bot>();
+builder.Services.AddSingleton<DiscordClient>();
+
+builder.Services.AddSingleton(new DiscordConfiguration
+{
+    TokenType = TokenType.Bot,
+    Token = Discord.Token,
+    Intents = DiscordIntents.All,
+    MinimumLogLevel = LogLevel.Information,
+    AutoReconnect = true,
+});
+
+builder.Services.AddLogging(x => x.AddConsole().SetMinimumLevel(LogLevel.Information));
+
+builder.Build().Run();
