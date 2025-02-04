c8d03d693807d36c5c01c788e92fdd9f0205c79d
Kaleb Garrett <kalebagarrett@gmail.com>
2025-02-03 20:39:39 -0500

    Integrated with Trivia API

---
+++ Triviabot.App/Models/TriviaModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Triviabot.App.Models;
+
+public class TriviaModel
+{
+    [JsonPropertyName("response_code")]
+    public int ResponseCode { get; set; }
+
+    [JsonPropertyName("results")]
+    public List<ResultModel> Results { get; set; }
+}
---
+++ Triviabot.App/Services/TriviaService.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using Triviabot.App.Models;
+
+namespace Triviabot.App.Services;
+
+public class TriviaService
+{
+    private readonly HttpClient _httpClient;
+
+    public TriviaService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<TriviaModel> Get(int category)
+    {
+       var result =  await _httpClient.GetAsync($"https://opentdb.com/api.php?amount=1&category={category}&type=multiple");
+
+       if (!result.IsSuccessStatusCode)
+       {
+           return null;
+       }
+       
+       var json = await result.Content.ReadAsStringAsync();
+       var root = JsonSerializer.Deserialize<TriviaModel>(json);
+
+       return root;
+    }
+}
