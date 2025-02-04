fac5fbb4b72e3191311cd62c64de68b65aea06df
Kaleb Garrett <kalebagarrett@gmail.com>
2025-02-04 16:01:27 -0500

    Added enum for question categories

---
+++ Triviabot.App/Slash Commands/Enums/Category.cs
@@ -0,0 +1,15 @@
+﻿using DSharpPlus.SlashCommands;
+
+namespace Triviabot.App.Slash_Commands.Enums;
+
+public enum Category
+{
+    [ChoiceName("General")] General = 9,
+    [ChoiceName("History")] History = 23,
+    [ChoiceName("Science")] Science = 17,
+    [ChoiceName("Animals")] Animals = 27,
+    [ChoiceName("Geography")] Geography = 22,
+    [ChoiceName("Sports")] Sports = 21,
+    [ChoiceName("Video Games")] VideoGames = 15,
+    [ChoiceName("Music")] Music = 12,
+}
